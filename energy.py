5f6c6f3fa103e77be5beb3d203226cdba7ddc575
Sarah Loftus <loftus.sj@gmail.com>
2013-08-19 19:25:25 +0100

    moved output to input page

--- energy.py
+++ energy.py
@@ -2,33 +2,31 @@ import webapp2
 from google.appengine.ext.webapp import template
 import datetime
 import models
+import time
 
 class MainPage(webapp2.RequestHandler):
 	
 	def get(self):
 
-		template_values = {'title': 'Home', 'today': datetime.date.today().isoformat()}
+		template_values = {'title': 'Home', 'today': datetime.date.today().isoformat(), 'entries': models.Entry.query()}
 
 		self.response.out.write(
 			template.render('form.html', template_values)
 			)
-
-class OutputPage(webapp2.RequestHandler):
 	
 	def post(self):
 
 		post_values = self.request.POST
-		post_values["title"] = "Output"
+
 		posted_date = datetime.datetime.strptime(post_values["date"], "%Y-%m-%d")
 		posted_units = float(post_values["units"])
 		entry = models.Entry(date=posted_date, units=posted_units)
 		entry.put()
+		# delay to hack eventual consistency of ndb
+		time.sleep(1)
 
-		self.response.out.write(
-			template.render('output.html', post_values)
-			)
+		self.redirect('/')
 
 application = webapp2.WSGIApplication([
 	('/', MainPage),
-	('/output', OutputPage),
 ], debug=True)
